Fix ic/ilu double precision.

tests/test_linalg_cuda_direct.py
@@ -15,18 +15,18 @@ all_methods = [
     pcg_cuda_csr_direct_ic,
     pcg_cuda_csr_direct_ainv,
 ]
-
+dtype = torch.float32
 # 3 1 0
 # 1 3 1
 # 0 1 3
 # Store in CSR
 outer_ptrs = torch.tensor([0, 2, 5, 7], dtype=torch.int32, device=torch.device('cuda'))
 inner_ptrs = torch.tensor([0, 1, 0, 1, 2, 1, 2], dtype=torch.int32, device=torch.device('cuda'))
-values = torch.tensor([3, 1, 1, 3, 1, 1, 3], dtype=torch.float32, device=torch.device('cuda'))
-b = torch.tensor([1, 2, 3], dtype=torch.float32, device=torch.device('cuda'))
+values = torch.tensor([3, 1, 1, 3, 1, 1, 3], dtype=dtype, device=torch.device('cuda'))
+b = torch.tensor([1, 2, 3], dtype=dtype, device=torch.device('cuda'))
 
 for m in all_methods:
-    x = torch.zeros(3, dtype=torch.float32, device=torch.device('cuda'))
+    x = torch.zeros(3, dtype=dtype, device=torch.device('cuda'))
     print(m.__name__, m(
       outer_ptrs=outer_ptrs,
       inner_indices=inner_ptrs,
@@ -42,8 +42,8 @@ N = 1024
 laplacian = grid_laplacian_nd_dbc([N, N])
 outer = torch.tensor(laplacian.indptr, dtype=torch.int32, device=torch.device('cuda'))
 inner = torch.tensor(laplacian.indices, dtype=torch.int32, device=torch.device('cuda'))
-values = torch.tensor(laplacian.data, dtype=torch.float32, device=torch.device('cuda'))
-b = torch.tensor(laplacian @ np.ones(N * N, dtype=np.float32), dtype=torch.float32, device=torch.device('cuda'))
+values = torch.tensor(laplacian.data, dtype=dtype, device=torch.device('cuda'))
+b = torch.tensor(laplacian @ np.ones(N * N, dtype=np.float32), dtype=dtype, device=torch.device('cuda'))
 
 def eval_once(m):
     x = torch.zeros_like(b)